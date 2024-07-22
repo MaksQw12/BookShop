222838760ba66ef08bda9f6b2246a8a9cfa54672
admin <admin@DESKTOP-387UJUJ>
2024-07-22 15:48:02 +0300

    123

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://26.197.135.59:7172/api/';
+export const API_URL = 'http://localhost:5104/api/';
 const $api = axios.create({
   baseURL: API_URL,
 });
